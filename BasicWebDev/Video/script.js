50d22b93039ac31c6f1844284eaaa3290d268e5f
BenThiry <148953910+BenThiry@users.noreply.github.com>
2023-12-17 15:31:30 -0600

    dsa
    dsa

---
+++ BasicWebDev/Video/script.js
@@ -0,0 +1,18 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  const videoFooter = document.getElementById("videoFooter");
+
+  window.addEventListener("scroll", function () {
+    const scrollPosition = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
+
+    // Set a threshold for when to show the video (adjust as needed)
+    const threshold = document.documentElement.scrollHeight - window.innerHeight - 100;
+
+    if (scrollPosition >= threshold) {
+      videoFooter.style.display = "block";
+    } else {
+      videoFooter.style.display = "none";
+    }
+  });
+});
